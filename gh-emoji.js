40b0b8129a0b533289ec8443656123cef776cd9b
Hector Leon Zarco Garcia <hezarco@gmail.com>
2016-04-04 00:25:05 +0200

    Install Eslint + Airbnb style

--- gh-emoji.js
+++ gh-emoji.js
@@ -1,9 +1,9 @@
 const enpoint = 'https://api.github.com/emojis';
-const delimiter_regex = /(\:[\w\.]*\:)/g;
+const delimiterRegex = /(\:[\w\.]*\:)/g;
 let emojis = null;
 
 export function load() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     if (emojis) return resolve(emojis);
 
     fetch(enpoint).then(r => r.json()).then((response) => {
@@ -28,13 +28,13 @@ export function getUrl(emojiId) {
 /**
  * TODO: Suport options
  * TODO: Suport custom classnames
- * @param  {String} text    
- * @param  {Object} options 
+ * @param {String} text    
+ * @param {Object} options 
  * @return {Promise}         
  */
 export function parse(text, options) {
   let output = '';
-  output += text.replace(delimiter_regex, (match, text, offset, string) => {
+  output += text.replace(delimiterRegex, (match, text, offset, string) => {
     let id = match.replace(/:/g, '');
     if (exist(id)) {
       let classNames = `gh-emoji gh-emoji-${id}`;
