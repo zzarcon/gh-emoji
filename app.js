b9f43e8681ba99391edecc326b043149d616ccf1
Oskar Cieslik (rpawfuiml) <oskar.cieslik@gmail.com>
2016-04-04 21:22:38 +0200

    Create autocomplete feature for demo editor

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-import {load as loadEmojis, parse as parseEmojis} from 'gh-emoji';
+import {load as loadEmojis, parse as parseEmojis, all} from 'gh-emoji';
+import TextComplete from 'textcomplete';
+import TextArea from 'textcomplete/lib/textarea';
+
+const AUTOCOMPLETE_COUNT = 5;
 
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  document.getElementById('editor').addEventListener('keyup', onKeyup);
-  loadEmojis().then(renderEditor);
+  const editorElem = document.getElementById('editor');
+
+  loadEmojis()
+    .then(renderEditor)
+    .then(() => editorElem.addEventListener('keyup', onKeyup))
+    .then(autocomplete);
 }
 
 function renderEditor() {
@@ -16,4 +24,30 @@ function renderEditor() {
 
 function onKeyup() {
   renderEditor();
+}
+
+function autocomplete() {
+  const unfinishedEmojiRegex = /(^|\s):(\w+)$/;
+  const emojiNames = Object.keys(all());
+
+  const editorElement = document.getElementById('editor');
+  const editor = new TextArea(editorElement);
+
+  const textcomplete = new TextComplete(editor, {
+    dropdown: {
+      maxCount: AUTOCOMPLETE_COUNT
+    }
+  });
+
+  textcomplete.register([{
+    match: unfinishedEmojiRegex,
+    search(term, callback) {
+      callback(
+        emojiNames
+          .filter(emoji => emoji.startsWith(term))
+          .map(emoji => `:${emoji}: ${parseEmojis(`:${emoji}:`)}`)
+          .sort()
+      );
+    }
+  }]);
 }
