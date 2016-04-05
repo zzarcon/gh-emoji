86e0bc0ed058ac87cc50a9ac355aca63cc0990a4
Oskar Cieslik (rpawfuiml) <oskar.cieslik@gmail.com>
2016-04-05 23:29:33 +0200

    New gh-emoji.js with jsdoc comments

--- dist/gh-emoji.js
+++ dist/gh-emoji.js
@@ -25,6 +25,19 @@
   var delimiterRegex = /(\:[\w\.]*\:)/g;
   var emojis = null;
 
+  /**
+   * Fetch the emoji data from Github's api.
+   *
+   * @example
+   * import {load as loadEmojis} from 'gh-emoji';
+   *
+   * loadEmojis().then((emojis) => {
+   *   console.log(emojis['+1']); // 👍
+   * });
+   *
+   * @returns {Promise<Object>} Promise which passes Object with emoji names
+   * as keys and generated image tags as values to callback.
+   */
   function load() {
     return new Promise(function (resolve) {
       if (emojis) return resolve(emojis);
@@ -38,18 +51,70 @@
     });
   }
 
+  /**
+   * Return all fetched emojis.
+   *
+   * @example
+   * import {load as loadEmojis, all as allEmojis} from 'gh-emoji';
+   *
+   * loadEmojis().then(() => {
+   *   console.log(allEmojis()); // {emojiName: emojiImageTag}
+   * });
+   *
+   * @returns {Object} Object with emoji names as keys and generated image tags
+   * as values.
+   */
   function all() {
     return emojis;
   }
 
+  /**
+   * Check if requested emoji exists.
+   *
+   * @example
+   * import {load as loadEmojis, exist as emojiExists} from 'gh-emoji';
+   *
+   * loadEmojis().then(() => {
+   *   console.log(emojiExists('foo')); // false
+   *   console.log(emojiExists('smile')); // true
+   * });
+   *
+   * @returns {Boolean}
+   */
   function exist(emojiId) {
     return !!all()[emojiId];
   }
 
+  /**
+   * Return github's image url of emoji.
+   *
+   * @example
+   * import {load as loadEmojis, getUrl} from 'gh-emoji';
+   *
+   * loadEmojis().then(() => {
+   *   console.log(getUrl('apple')); // 'https://assets-cdn.github.com/images/icons/emoji/unicode/1f34e.png?v6'
+   * });
+   *
+   * @returns {String} Image url of given emoji.
+   */
   function getUrl(emojiId) {
     return all()[emojiId];
   }
 
+  /**
+   * Parse text and replace emoji tags with actual emoji symbols.
+   *
+   * @example
+   * import {load as loadEmojis, parse} from 'gh-emoji';
+   *
+   * const text = 'Do you believe in :alien:...? :scream:';
+   *
+   * loadEmojis().then(() => {
+   *   console.log(parse(text)) // 'Do you believe in 👽...? 😱';
+   * });
+   *
+   * @returns {String} Parsed text with emoji image tags in it.
+   */
   function parse(text) {
     var output = '';
     output += text.replace(delimiterRegex, function (match) {
